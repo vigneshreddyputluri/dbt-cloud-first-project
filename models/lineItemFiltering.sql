cd5fd43a656c43f64a85c88d241dd559ae4928b6
Vignesh Putluri <pvreddyp4@gmail.com>
2026-05-16 17:39:13 +0000

    Syncing

--- models/lineItemFiltering.sql
+++
@@ -1,3 +0,0 @@
-select l_shipmode, count(*) Count, count(*)/(select Count(*) from {{ source('sf1000', 'LINEITEM') }})*100 Proportion
-from {{ source('sf1000', 'LINEITEM') }}
-group by L_shipmode
--- models/secondTable.sql
+++
@@ -1,3 +0,0 @@
-{{config(materialized="view")}}
-
-select 2 as col
